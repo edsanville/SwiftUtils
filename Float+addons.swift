cfd19d378af00f731df794f50eca125d5ceb9854
Ed Sanville <edsanville@gmail.com>
2019-09-07 16:47:35 -0400

    Add Float.from(stringOrNumber:)

---
+++ Float+addons.swift
@@ -0,0 +1,33 @@
+//
+//  Float+addons.swift
+//  StarMap3DPlus
+//
+//  Created by Edward Sanville on 9/7/19.
+//
+
+import Foundation
+
+extension Float {
+    
+    static func from(stringOrNumber: Any?) -> Float? {
+        
+        if stringOrNumber == nil {
+            return nil
+        }
+        
+        if let s = stringOrNumber as? String {
+            return Float(s)
+        }
+
+        if let f = stringOrNumber as? Float {
+            return f
+        }
+
+        if let d = stringOrNumber as? Double {
+            return Float(d)
+        }
+
+        return nil
+    }
+    
+}
