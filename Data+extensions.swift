525685f9d57a59543729a9fbe28440993a15f3a0
Ed Sanville <edsanville@gmail.com>
2020-02-21 17:25:15 -0500

    Allow zero-length random Data

--- Data+extensions.swift
+++ Data+extensions.swift
@@ -13,7 +13,7 @@ extension Data {
     static func withRandomBytes(_ count: Int) -> Data {
         var data = Data()
         
-        for _ in 1...count {
+        for _ in 0..<count {
             data.append(UInt8.random(in: UInt8.min...UInt8.max))
         }
         
