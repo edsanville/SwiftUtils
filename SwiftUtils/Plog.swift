674d06bacd5ddb887b8a1a0ce75ee8294c985d98
Ed Sanville <edsanville@gmail.com>
2020-05-06 21:44:16 -0400

    Allow plog to save all its logs to retrieve as a String or as Data

--- SwiftUtils/Plog.swift
+++ SwiftUtils/Plog.swift
@@ -8,12 +8,54 @@
 
 import Foundation
 
-func plog(_ str: String, file_path: String = #file, line: Int = #line) {
-    let file = URL(fileURLWithPath: file_path).lastPathComponent
-    print("✅ \(file) (\(line)): \(str)")
+@objcMembers
+final class Plog: NSObject {
+
+    static var saveLogs = true
+    private static var log = ""
+
+    class func append(line: String) {
+        log.append(line + "\n")
+    }
+
+    class func string() -> String {
+        return log
+    }
+
+    class func data() -> Data {
+        return log.data(using: .utf8) ?? "Couldn't encode plog to utf8!".data(using: .utf8)!
+    }
+
+    class func clear() {
+        log = ""
+    }
+
+    class func plog(_ str: CustomStringConvertible, file_path: String = #file, line: Int = #line) {
+        let file = URL(fileURLWithPath: file_path).lastPathComponent
+        let line = "✅ \(file) (\(line)): \(str)"
+        print(line)
+
+        if Plog.saveLogs {
+            Plog.append(line: line)
+        }
+    }
+
+    class func perr(_ str: CustomStringConvertible, file_path: String = #file, line: Int = #line) {
+        let file = URL(fileURLWithPath: file_path).lastPathComponent
+        let line = "🛑 \(file) (\(line)): \(str)"
+        print(line)
+
+        if Plog.saveLogs {
+            Plog.append(line: line)
+        }
+    }
+
 }
 
-func perr(_ str: String, file_path: String = #file, line: Int = #line) {
-    let file = URL(fileURLWithPath: file_path).lastPathComponent
-    print("🛑 \(file) (\(line)): \(str)")
+func plog(_ str: CustomStringConvertible, file_path: String = #file, line: Int = #line) {
+    Plog.plog(str, file_path: file_path, line: line)
+}
+
+func perr(_ str: CustomStringConvertible, file_path: String = #file, line: Int = #line) {
+    Plog.perr(str, file_path: file_path, line: line)
 }
