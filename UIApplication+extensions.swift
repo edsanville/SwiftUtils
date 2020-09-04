23efc5ab42f1f42a4289686b495a6431399f59d8
Ed Sanville <edsanville@gmail.com>
2020-09-04 11:13:15 -0400

    Added UIApplcation extension

---
+++ UIApplication+extensions.swift
@@ -0,0 +1,32 @@
+//
+//  UIApplication+extensions.swift
+//  StarMap3DPlus
+//
+//  Created by Edward Sanville on 4/12/19.
+//
+
+import Foundation
+
+@objc
+extension UIApplication {
+    
+    static func appFullName() -> String {
+        return Bundle.main.localizedInfoDictionary?["AppFullName"] as? String ?? (Bundle.main.infoDictionary!["AppFullName"] as? String)!
+    }
+    
+    static func appName() -> String {
+        return Bundle.main.localizedInfoDictionary?["CFBundleDisplayName"] as? String ?? (Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String)!
+    }
+    
+    static func currentLanguage() -> String {
+        return (UserDefaults.standard.object(forKey: "AppleLanguages") as! NSArray).firstObject as! String
+    }
+    
+    static func appVersion() -> String {
+        return Bundle.main.infoDictionary!["CFBundleShortVersionString"] as? String ?? ""
+    }
+
+    static func appBuild() -> String {
+        return Bundle.main.infoDictionary!["CFBundleVersion"] as? String ?? ""
+    }
+}
